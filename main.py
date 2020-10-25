a3524081bf1a81614bede1ec50f3e5d26e4304f4
Anna Wamej-Stolc <a.wamej@gmail.com>
2020-10-25 11:03:35 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("zadanie_2")
+num_list = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
+new_list1 = num_list[1:4] + num_list[5:10] + num_list[-2:]
+print(new_list1)
+new_list2 = list([num_list[0]]) + list([num_list[4]]) + num_list[-4:-2]
+print(new_list2)
+# sprawdzenie
+check = new_list1 + new_list2
+print("Sprawdzenie: " + str(bool(sorted(num_list) == sorted(check))))
