5e8315adecebeb4d96b43c997ed8cb00ab0195de
Anna Wamej-Stolc <a.wamej@gmail.com>
2020-10-25 18:07:48 +0100

    Remove sprawdzenie

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ new_list1 = num_list[1:4] + num_list[5:10] + num_list[-2:]
 print(new_list1)
 new_list2 = list([num_list[0]]) + list([num_list[4]]) + num_list[-4:-2]
 print(new_list2)
-# sprawdzenie
-check = new_list1 + new_list2
-print("Sprawdzenie: " + str(bool(sorted(num_list) == sorted(check))))
+
